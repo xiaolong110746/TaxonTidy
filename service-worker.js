672099752ec7258b58b27bbf291801b624832995
xiaolong110746 <158761093+xiaolong110746@users.noreply.github.com>
2025-05-21 22:34:31 +0800

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// 缓存名称
+const CACHE_NAME = 'fungal-counter-v1';
+// 需要缓存的文件列表
+const urlsToCache = [
+    'index.html',
+    'manifest.json',
+    'icon-192x192.png',
+    'icon-512x512.png'
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+           .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// 激活 Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+           .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.filter(cacheName => cacheName!== CACHE_NAME)
+                       .map(cacheName => caches.delete(cacheName))
+                );
+            })
+    );
+});
+
+// 拦截请求并返回缓存内容
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+           .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
